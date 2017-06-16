8dcb9d3601ebbac90ba70cbb24c8826011a1e87f
Anthony Watson <Adubya@Anthonys-MacBook-Air.local>
2017-06-16 15:26:17 -0400

    up to sequlize

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express     = require( 'express' );
 const app         = express(); // creates instance of express application
 const nunjucks    = require('nunjucks');
-const routes      = require('./routes');
+// const routes      = require('/routes');
 const bodyParser  = require('body-parser');
 const morgan = require('morgan');
+const models = require('/models');
+
 morgan(':method :url :status :res[content-length] - :response-time ms')
 
 
 app.use(express.static('public'));
-
 // ------ nunjucks ------
 
 app.set('view engine', 'html');
@@ -20,7 +21,7 @@ nunjucks.configure('views', { noCache: true });
 // app.use(bodyParser.json({type: '*+json'}));
 
 
-app.use('/', routes); //creates middleware
+// app.use('/', routes); //creates middleware
 
 app.use('/', (req, res, next) => {
   console.log(req.method + ' ' + req.url);
@@ -28,4 +29,12 @@ app.use('/', (req, res, next) => {
   });
 
 // =============// =============// =============// =============
-app.listen(3000, () => {console.log('server listening');})
+
+
+models.sync()
+.then( () => {
+    app.listen(3000, () => {
+        console.log("We\'re listening!");
+    });
+})
+.catch(console.error);
--- models/index.js
+++ models/index.js
@@ -1,2 +1,33 @@
-let Sequelize = require('sequelize');
-let sequelize = new Sequelize('postgres://localhost/3000');
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:3000/wikistack');
+
+let Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING
+    },
+    urlTitle: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
+})
+
+let User = db.define('user', {
+    name: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+
+
+module.exports = {
+  Page: Page,
+  User: User
+};
